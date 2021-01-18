d178e07744deaa31bcfa8f973ae50ce8fef32192
toru0209 <76867451+toru0209@users.noreply.github.com>
2021-01-18 23:02:24 +0900

    既読機能のフロントエンド実装

--- app/javascript/packs/checked.js
+++
@@ -1,8 +0,0 @@
-function check() {
-  const posts = document.querySelectorAll(".post");
-  posts.forEach(function (post) {
-    post.addEventListener("click", () => { });
-  });
-}
-window.addEventListener("load", check);
-
